8e527af973180347493f5305ec922e056311d29b
Javier Martín <100529062+jotaeme890@users.noreply.github.com>
2024-04-09 20:16:04 +0200

    Delete TFCChato directory

--- TFCChato/src/app/core/services/media.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-
-@Injectable({
-    providedIn: 'root'
-})
-export abstract class MediaService {  
-    public abstract upload(blob:Blob):Observable<any[]>;
-}

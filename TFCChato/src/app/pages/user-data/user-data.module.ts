e9fe808ce7f98c9945dc13d27586b449c9648a42
jotaeme890 <permanentjavi@gmail.com>
2024-04-22 16:58:30 +0200

    Added: User data page, translated words

--- TFCChato/src/app/pages/user-data/user-data.module.ts
+++ TFCChato/src/app/pages/user-data/user-data.module.ts
@@ -7,13 +7,24 @@ import { IonicModule } from '@ionic/angular';
 import { UserDataPageRoutingModule } from './user-data-routing.module';
 
 import { UserDataPage } from './user-data.page';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { createTranslateLoader } from 'src/app/core/translate/translate';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    UserDataPageRoutingModule
+    UserDataPageRoutingModule, 
+    // Translate
+    TranslateModule.forChild({
+      loader: {
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [HttpClient]
+      }
+    }),
   ],
   declarations: [UserDataPage]
 })

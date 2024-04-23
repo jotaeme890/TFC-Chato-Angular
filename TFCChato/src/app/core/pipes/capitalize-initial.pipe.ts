a1f0a02cf26c22333898a378e2e45d350e865a01
jotaeme890 <permanentjavi@gmail.com>
2024-04-23 17:13:29 +0200

    Fixed: Comments

--- TFCChato/src/app/core/pipes/capitalize-initial.pipe.ts
+++ TFCChato/src/app/core/pipes/capitalize-initial.pipe.ts
@@ -6,15 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class CapitalizeInitialPipe implements PipeTransform {
 
   /**
-   * The function takes a string as input, capitalizes the first letter, and adds a period at the end.
-   * 
-   * @param value The `value` parameter in the `transform` function is a string or undefined. If the
-   * `value` is undefined or falsy, the function returns an empty string. Otherwise, it capitalizes the
-   * first character of the string and appends a period at the end before returning the transformed
-   * string.
-   * @return An empty string is being returned if the `value` is falsy. Otherwise, the first character
-   * of the `value` is capitalized and followed by a period.
-   */
+  * The function takes a string as input, capitalizes the first letter, and adds a period at the end.
+  * 
+  * @param value The `value` parameter in the `transform` function is a string or undefined. If the
+  * `value` is undefined or falsy, the function returns an empty string. Otherwise, it capitalizes the
+  * first character of the string and appends a period at the end before returning the transformed
+  * string.
+  * @return An empty string is being returned if the `value` is falsy. Otherwise, the first character
+  * of the `value` is capitalized and followed by a period.
+  */
   transform(value: string | undefined): string {
     if (!value) {
       return '';

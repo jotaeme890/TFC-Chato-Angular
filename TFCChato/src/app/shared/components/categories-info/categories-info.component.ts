7e3753b5b87b60907761fb2165e989ffb0bdeb83
jotaeme890 <permanentjavi@gmail.com>
2024-05-22 14:24:02 +0200

    Fixed: Gap

--- TFCChato/src/app/shared/components/categories-info/categories-info.component.ts
+++ TFCChato/src/app/shared/components/categories-info/categories-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { CategoryInfo } from 'src/app/core/interfaces/category-info';
 
 @Component({
@@ -8,20 +8,34 @@ import { CategoryInfo } from 'src/app/core/interfaces/category-info';
 })
 export class CategoriesInfoComponent  implements OnInit {
 
+  isScreenSmall: boolean = false;
   @Input() categories: CategoryInfo[] | null | undefined;
   @Output() onTrashClicked: EventEmitter<CategoryInfo> = new EventEmitter<CategoryInfo>()
   @Output() onEditClicked: EventEmitter<string> = new EventEmitter<string>()
 
 
-  constructor() { }
+  constructor() {
+    this.checkScreenSize(window.innerWidth);
+   }
 
   ngOnInit() {}
-  openModal(){
-
-  }
 
   deleteCategory( category: CategoryInfo ) {
     this.onTrashClicked.emit(category)
   }
+
+  editCategory() {
+
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenSize(event.target.innerWidth);
+  }
+
+  private checkScreenSize(width: number) {
+    this.isScreenSmall = width <= 700;
+  }
+
 
 }

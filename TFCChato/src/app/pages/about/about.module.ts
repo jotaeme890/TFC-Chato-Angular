fdc1e87929fc7dc667d9d79bb49dc1f12b9cbc82
jotaeme890 <permanentjavi@gmail.com>
2024-06-10 18:15:44 +0200

    Quick fix

--- TFCChato/src/app/pages/about/about.module.ts
+++ TFCChato/src/app/pages/about/about.module.ts
@@ -7,13 +7,24 @@ import { IonicModule } from '@ionic/angular';
 import { AboutPageRoutingModule } from './about-routing.module';
 
 import { AboutPage } from './about.page';
+import { HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { createTranslateLoader } from 'src/app/core/translate/translate';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AboutPageRoutingModule
+    AboutPageRoutingModule,
+    // Translate
+    TranslateModule.forChild({
+      loader: {
+      provide: TranslateLoader,
+      useFactory: (createTranslateLoader),
+      deps: [HttpClient]
+      }
+    }),
   ],
   declarations: [AboutPage]
 })
